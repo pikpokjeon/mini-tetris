5920ab8bbcfe475d2f071ea3f9cd4f473e19a6b5
pikpokjeon <pikpokjeon@gmail.com>
2021-07-03 18:47:27 +0900

    feature: 테트리스에 필요한 항목 중간정리

--- src/tetris/index.js
+++ src/tetris/index.js
@@ -27,6 +27,75 @@ const genElement = (type, attr, animate) =>
     return type
 }
 
+const store = (initData) =>
+{
+    let innnerState = Object.assign({}, initData)
+
+    const Publish = (topic, obj) =>
+    {
+        for (const [k, v] of Object.entries(obj))
+        {
+            Reflect.set(innnerState[topic], k, v)
+        }
+    }
+    const Use = topic => Object.assign({}, innnerState[topic])
+
+    return { Publish, Use }
+}
+
+const initData = {
+    'currentBlock': {
+        position: {
+            center: [],
+            edge: { // x, y
+                left: [],
+                right: [],
+                bottom: [], y
+            },
+            gap: {
+                left: {
+                    left: [],
+                    bottom: [],
+                },
+                right: {
+                    right: [],
+                    bottom: []
+                },
+                center: {
+                    bottom: []
+                },
+            },
+        },
+        status: {
+            type: -1,
+            spin: -1,
+            parts: [], //id
+            index: [],
+            color: -1,
+        }
+    },
+    'play': {
+        status: {
+            level: -1,
+            isDropped: false,
+            readyDrop: true,
+            isSpin: {
+                spinToMove: {
+                    direction: -1.
+                }
+            },
+            round: -1,
+            hit: -1,
+            line: {
+                stacked: -1,
+                toRemove: -1,
+                left: -1,
+                toGetStacked: -1,
+            },
+        }
+    }
+}
+
 
 const renders = () =>
 {
@@ -36,19 +105,23 @@ const renders = () =>
         W: 385,
         BLOCK: 35,
     }
+    const current = [35, 6]
     const d = Array(46).fill(35).map((e, i) => e * i)
-    const size = genSize(385, d)
     const svg = genElement('svg', { height: [SIZE.H], width: [SIZE.W], fill: 'black', style: 'overflow:visible' })
     const group = genElement('g', { height: [SIZE.H], width: [SIZE.W], fill: 'black', style: 'overflow:visible' })
+    const size = genSize(385, d)
+
+    // const background = () 
+
     let rect
     const g = genElement('g', { height: [SIZE.H], width: [SIZE.W], fill: 'black', style: 'overflow:visible' })
 
-    for (let k = 0; k < 35; k++)
+    for (let k = 35; k > -1; k--)
     {
-        for (let i = 0; i < 11; i++)
+        for (let i = 0; i <= 11; i++)
         {
 
-            rect = genElement('rect', { height: [SIZE.BLOCK], width: [SIZE.BLOCK], fill: 'grey', x: size.x(i), y: size.y(d[k]) })
+            rect = genElement('rect', { id: `${k}-${i}`, height: [SIZE.BLOCK], width: [SIZE.BLOCK], fill: 'grey', x: size.x(i), y: size.y(d[k]) })
             g.appendChild(rect)
         }
 
